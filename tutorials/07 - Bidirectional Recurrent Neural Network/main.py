1862d0fb28871b7ef7f63e26af8028b080d17018
Ke Ding <kedingpku@gmail.com>
2017-03-16 15:44:09 +0800

    make RNN definition independent of global variables

--- tutorials/07 - Bidirectional Recurrent Neural Network/main.py
+++ tutorials/07 - Bidirectional Recurrent Neural Network/main.py
@@ -38,14 +38,16 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 class BiRNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(BiRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, 
                             batch_first=True, bidirectional=True)
         self.fc = nn.Linear(hidden_size*2, num_classes)  # 2 for bidirection 
     
     def forward(self, x):
         # Set initial states
-        h0 = Variable(torch.zeros(num_layers*2, x.size(0), hidden_size)) # 2 for bidirection 
-        c0 = Variable(torch.zeros(num_layers*2, x.size(0), hidden_size))
+        h0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size)) # 2 for bidirection 
+        c0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size))
         
         # Forward propagate RNN
         out, _ = self.lstm(x, (h0, c0))
