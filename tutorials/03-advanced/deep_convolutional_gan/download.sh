8f459d745d80cf6396df3c4bfaf2a7096552b975
yunjey <yunjey47@naver.com>
2017-06-23 12:20:05 +0900

    Update download.sh

--- tutorials/03-advanced/deep_convolutional_gan/download.sh
+++ tutorials/03-advanced/deep_convolutional_gan/download.sh
@@ -1,2 +1,2 @@
-wget https://www.dropbox.com/s/e0ig4nf1v94hyj8/CelebA.zip?dl=0 -P ./
-unzip CelebA.zip -d ./
+wget https://www.dropbox.com/s/e0ig4nf1v94hyj8/CelebA_128crop_FD.zip?dl=0 -P ./
+unzip CelebA_128crop_FD.zip -d ./
