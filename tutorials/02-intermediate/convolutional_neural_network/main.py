e653bf96e0e394eff2f5d5c4819819870cd74296
mtsadler <msadler@umass.edu>
2018-05-14 15:44:55 -0400

    fixed dataset loader path
    Before I had data directory located one directory higher, but really it should have been two directories higher.

--- tutorials/02-intermediate/convolutional_neural_network/main.py
+++ tutorials/02-intermediate/convolutional_neural_network/main.py
@@ -1,12 +1,8 @@
-import torch 
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-
-
+from torch.autograd import Variable
 # Device configuration
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
 # Hyper parameters
 num_epochs = 5
 num_classes = 10
@@ -14,23 +10,24 @@ batch_size = 100
 learning_rate = 0.001
 
 # MNIST dataset
-train_dataset = torchvision.datasets.MNIST(root='../../data/',
-                                           train=True, 
+train_dataset = torchvision.datasets.MNIST(root='/../../data/',
+                                           train=True,
                                            transform=transforms.ToTensor(),
                                            download=True)
 
-test_dataset = torchvision.datasets.MNIST(root='../../data/',
-                                          train=False, 
+test_dataset = torchvision.datasets.MNIST(root='/../../data/',
+                                          train=False,
                                           transform=transforms.ToTensor())
 
 # Data loader
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size, 
+                                           batch_size=batch_size,
                                            shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size, 
+                                          batch_size=batch_size,
                                           shuffle=False)
+
 
 # Convolutional neural network (two convolutional layers)
 class ConvNet(nn.Module):
@@ -46,16 +43,17 @@ class ConvNet(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Linear(7*7*32, num_classes)
-        
+        self.fc = nn.Linear(7 * 7 * 32, num_classes)
+
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
-        out = out.reshape(out.size(0), -1)
+        out = out.view(out.size(0), -1)
         out = self.fc(out)
         return out
 
-model = ConvNet(num_classes).to(device)
+
+model = ConvNet(num_classes)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -65,36 +63,32 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 total_step = len(train_loader)
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
-        images = images.to(device)
-        labels = labels.to(device)
-        
+        images = Variable(images)
+        labels = Variable(labels)
         # Forward pass
         outputs = model(images)
         loss = criterion(outputs, labels)
-        
+
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+        if (i + 1) % 100 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                   .format(epoch + 1, num_epochs, i + 1, total_step, loss.data[0]))
 
 # Test the model
 model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+correct = 0
+total = 0
+for images, labels in test_loader:
+    images = Variable(images)
+    outputs = model(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum()
 
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
 
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
